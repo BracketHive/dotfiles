d4b92f33bf0f481a58b867f3262c275543879c99
BracketHive <info@brackethive.com>
2025-01-03 12:22:25 +0200

    nvim snacks changes

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -9,21 +9,18 @@ return {
       sections = {
         { section = "header" },
         { section = "keys", gap = 1, padding = 1 },
-        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { section = "startup" },
         {
           section = "terminal",
+          -- This needs installation
           cmd = "ascii-image-converter ~/.config/nvim/brackethive.png -C -c",
           random = 10,
           pane = 2,
           indent = 4,
-          height = 50,
-          width = 50,
+          height = 30,
         },
-        { section = "startup" },
       },
     },
-    indent = { enabled = false },
-    input = { enabled = true },
     notifier = {
       enabled = true,
       timeout = 3000,
